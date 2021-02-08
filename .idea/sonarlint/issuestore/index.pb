dba20f84fdcf8ab97b878a3d9528fe681e0736a2
AlexD <alexander.drugakov@gmail.com>
2021-02-08 18:10:23 +0300

    0.31v. Search frame added.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+v
+Fapp/src/main/java/com/example/electronicdiary/login/LoginActivity.java,d\8\d8ba1847334b3a27b3e99ae38b4531bac754abc6
+o
+?app/src/main/java/com/example/electronicdiary/MainActivity.java,0\0\0021d279a6bc2b8f817d2632275f750f19d7c3c2
--- app/src/main/java/com/example/electronicdiary/profile/ProfileFragment.java
+++ app/src/main/java/com/example/electronicdiary/profile/ProfileFragment.java
@@ -4,31 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.electronicdiary.R;
 
 public class ProfileFragment extends Fragment {
-
-    private ProfileViewModel profileViewModel;
+    //private SubjectsAdapter subjectsAdapter;
+    //private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
+
+        //TODO запуск поиска
+        /*subjectsAdapter = new SubjectsAdapter(getActivity().getApplicationContext(), subjects);
+
+        final RecyclerView recyclerView = root.findViewById(R.id.searchedStudentsList);
+        recyclerView.setAdapter(subjectsAdapter);
+        recyclerView.addOnItemTouchListener(getClickListener());
+        */
+        
+        /*profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                
             }
-        });
+        });*/
+
         return root;
     }
+
+    /*private RecyclerItemClickListener getClickListener() {
+
+        return new RecyclerItemClickListener(this, heroesView, new RecyclerItemClickListener.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+                //not used
+            }
+        });
+    }*/
 }
