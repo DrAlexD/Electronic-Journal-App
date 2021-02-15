78b12005df8690f3e19ec8f2695626bedfe28f4e
DrAlexD <alexander.drugakov@gmail.com>
2021-02-15 03:41:09 +0300

    0.55v. Added students profiles.

--- app/src/main/java/com/example/electronicdiary/group/ModulesPagerAdapter.java
+++ app/src/main/java/com/example/electronicdiary/group/ModulesPagerAdapter.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 
@@ -27,10 +26,5 @@ class ModulesPagerAdapter extends FragmentStateAdapter {
     @Override
     public int getItemCount() {
         return 3;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return PagerAdapter.POSITION_NONE;
     }
 }
